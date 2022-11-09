add29d7eeef92b8554886d8e12ebb3c3ece07307
Lance_0000 <lcblows3@gmail.com>
2022-11-09 15:45:15 +0200

    Second commit

---
+++ Employee.cpp
@@ -0,0 +1,50 @@
+// Abstract-base-class Employee
+#include <iostream>
+#include "Employee.h"
+using namespace std;
+
+Employee::Employee( const string &first, const string &last, const string &ssn)
+	: firstName( first ), lastName( last ), socialSecurityNumber (ssn )
+{
+
+}
+
+// set first name
+void Employee::setFirstName( const string &first )
+{
+	firstName = first;
+}
+
+void Employee:getFirstName() const
+{
+	return firstName;
+}
+
+// set last name
+void Employee::setLastName( const string &last )
+{
+	lastName = last;
+}
+
+// return last name
+void Employee::getLastName() const 
+{
+	return lastName;
+}
+
+void Employee::setSocialSecurityNumber( const string &ssn )
+{
+	socialSecurityNumber = ssn;
+}
+
+void Employee::getSocialSecurityNumber() const
+{
+	return socialSecurityNumber;
+}
+
+// print Employees info (virtual, but not pure virtual)
+void Employee::print() const
+{
+	cout << getFirstName() << ' ' << getLastName()
+		<< "\nsocial security number: " << getSocialSecurityNumber();
+}
