8b63555cfc1066f66877b2af384567bb8c8e8daf
Lance_0000 <lcblows3@gmail.com>
2022-11-09 15:15:59 +0200

    initial commit

---
+++ Employee.h
@@ -0,0 +1,30 @@
+#define EMPLOYEE_H
+
+#include <string>
+
+class Employee
+{
+public:
+	Employee( const std::string &, const std::string &, const std::string & );
+	virtual ~Employee() { } // virtual destructor
+							
+	void setFirstName( const std::string & );
+	std::string getFirstName() const;
+
+	void setLastName( const std::string & );
+	std::string getLastName() const;
+
+	void setSocialSecurityNumber( const std::string & );
+	std::string getSocialSecurityNumber() const;
+
+	// pure virtual function makes Employee an abstract base class
+	virtual double earnings() const = 0; // pure virtual
+	virtual void print() const; // virtual
+								
+private:
+	std::string firstName;
+	std::string lastName;
+	std::string socialSecurityNumber;
+};
+
+#endif
